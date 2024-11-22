eb2d47f6accf7efc01dfede1c9215e13071437c1
zxcvwrs <wolertr@gmail.com>
2024-11-22 22:46:25 +0100

    Zadanie 1 - fix

--- Python/Flask_Book_Library/project/customers/models.py
+++ Python/Flask_Book_Library/project/customers/models.py
@@ -22,7 +22,7 @@ class Customer(db.Model):
         print("Getting: " + str(self),flush=True)
 
     def __repr__(self):
-        return f"Customer(ID: {self.id}, Name: {self.name}, City: {self.city}, Age: {self.age}, Pesel: {self.pesel}, Street: {self.street}, AppNo: {self.appNo})"
+        return f"Customer(ID: {self.id}, Name: {self.name}, City: {'*' * len(self.city)}, Age: {self.age}, Pesel: {'*' * len(self.pesel)}, Street: {'*' * len(self.street)}, AppNo: {'*' * len(self.appNo)})"
 
 
 with app.app_context():
